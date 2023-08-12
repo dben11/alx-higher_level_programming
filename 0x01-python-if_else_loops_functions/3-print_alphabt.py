4a9fea9c3c891c4fe996dd3b03ded0785a1837ab
dben <sam.dben@gmail.com>
2023-08-12 15:38:13 -0700

    write an ascii program that prints an alphabet in lower case

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in 'abcdefghijklmnopqrstuvwxyz':
-    if(i != 'e' and i != 'q'):
-        print(f"{0}".format(i), end="")
+    if (i != 'e' and i != 'q'):
+        print(i"{0}".format(i), end="")
