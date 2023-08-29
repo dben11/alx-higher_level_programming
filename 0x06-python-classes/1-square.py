f587bac9288a3bb2b0d4a5731b67571ec5f7e446
dben <sam.dben@gmail.com>
2023-08-29 13:53:29 -0700

    working with classes in oop

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Working with oop in python"""
+
+class Square():
+    """Implementing the square class"""
+    def _int_(self, size):
+        """ initialize the class
+        Args:
+            size (int): size of the square
+        """
+        self._size = size
