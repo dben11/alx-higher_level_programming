556e85899b5c01ee13d8c5f2199540149c4cd3c9
dben <sam.dben@gmail.com>
2023-10-18 12:24:20 -0700

    delete databases

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- How to delete the database htbn_0c_0 in your MYQSL sever
+-- If the database hbtn_0c_0 doesn't exist,script should not fail.
+-- You are not allowed to use the SELECT os SHOW statements
+DELETE DATABASE hbtn_0c_0;
