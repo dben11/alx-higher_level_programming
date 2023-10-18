36ee33603fecdff07f300b0dbc449631e6e01fb3
dben <sam.dben@gmail.com>
2023-10-18 12:48:33 -0700

    createtables

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,7 @@
--- Create a table called first_table
+-- Create a table called first_table in the current databases
 -- first_table description
+--              id INT
+--              name VARCHAR(256)
 -- The database name will be passed an argument of the mysql command.
 -- If the table first_table already exitss,your script should not fail
 -- You are not allowed to se the SELECT or SHOW statements.
