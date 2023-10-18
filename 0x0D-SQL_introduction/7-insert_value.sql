d7223589fbfc0c6bcaab80a41ff5e538a9bdf873
dben <sam.dben@gmail.com>
2023-10-18 15:53:58 -0700

    insert discription

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- Insert a new row in the table first_table (database hbntn_)c_0) in your MYSQL server
+-- New row
+--       id = 89
+--       name = Best School
+-- The database name will be passed as an argument of the mysql command
+INSERT INTO `first_table` (`id`,` name`) VALUE (89, 'Best School');
