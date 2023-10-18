b9ec03f0faf992808901588243266a555bf847c9
dben <sam.dben@gmail.com>
2023-10-18 15:40:28 -0700

    full discription

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- print the full discription of the table first_table from the database
+-- the database name will be passed as an argument of mysql command
+-- You are not allow to use the DESCRIBE or EXPLAIN statement
+SHOW CREATE TABLE first_table;
