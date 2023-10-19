67fd2a66ee249a8bda0d568687d577188239ad77
dben <sam.dben@gmail.com>
2023-10-19 02:57:40 -0700

    Number by score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+--lists the number of records with thesame scores in the table
+--The result should display:
+--          the scores
+--          the number of records for this score with the label number
+-- The list should be sorted by the number of records(descending)
+--the database name will be passed as an argument to the mysql command
+
+SELECT score, COUNT(*) as number FROM second_table GROUP BY ORDER number DESC;
