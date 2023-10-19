0c370941b6d81a496287b663ae2c2db7c9a68f91
dben <sam.dben@gmail.com>
2023-10-19 03:14:06 -0700

    Number by score

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,1 +1,6 @@
--- th
+-- lists all records of the table second_table of the database
+-- Don't list rows without name value
+-- Result should display the score and name (in this order)
+-- Records should be listed by descending score
+-- The database name will be passed as an argument to be mysql command
+SELECT scoree, name FROM second_table WHERE name IS NULL AND name != '' ORDER BY score DESC;
