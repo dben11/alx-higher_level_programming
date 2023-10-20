f7bac35b5cb49fa83e43966321972b0f646560a9
dben <sam.dben@gmail.com>
2023-10-19 17:01:52 -0700

    my privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 -- lists all privileges of the mysql user user_0d_1 and user_0d_2 on your server(in localhost).
-GRANT CREATE,ALTER,DROP,INSERT,UPDATE,DELETE,SELECT,REFERENCES,FUSH,RELOAD on *.* TO 'user_0d_1'AND 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT CREATE,ALTER,DROP,INSERT,UPDATE,DELETE,SELECT,REFERENCES,FUSH,RELOAD on *.* TO 'user_0d_1'AND'user_0d_2'@'localhost' WITH GRANT OPTION;
