ebf04cb1d2d952253df77874d33f519bad50987a
dben <sam.dben@gmail.com>
2023-10-19 18:20:52 -0700

    id not null

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- create the database hbtn_0d_usa and the table cities.
+-- cities description:
+-- if the database hbtn_0d_usa already exists, your script should not fail
+-- if the table cities already exists, your script should not fail
+
+CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
+CREATE TABLE IF NOT EXIST htbn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO-INCREMENT,
+state_id INT NOT NULL,
+NAME varchar(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCE htbn_0d_usa.states(id));
