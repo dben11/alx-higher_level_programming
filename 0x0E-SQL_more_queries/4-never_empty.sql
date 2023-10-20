8c13314594bbda3cec7fdc20d3b89281aef59af4
dben <sam.dben@gmail.com>
2023-10-19 17:55:25 -0700

    id not null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,4 @@
 -- the database name will be passed as an argument of the mysql command
 -- if the table id_not_null already exists, your script should not fail
 
-CREATE TABLE IF NOT EXISTS id_not_null (id INT, name VARCHAR(256) value (1));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
