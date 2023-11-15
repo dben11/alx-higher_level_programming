2e9fad2569bdd8877e9ca9d5b405a72837e29bb3
dben <sam.dben@gmail.com>
2023-11-15 12:38:44 -0800

    integers

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const num = Math.floor(Number(process.argv[2]));
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log(`my number:${num}`);
+  console.log(`my number: ${num}`);
 }
