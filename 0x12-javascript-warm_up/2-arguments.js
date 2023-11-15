ed5af994536b1a8ffb113453a8fc58157ad40fe4
dben <sam.dben@gmail.com>
2023-11-15 04:00:34 -0800

    variables

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 
+const arr = process.arg.lenght;
+if(arr ==2)
+{
 console.log('No argument');
+}
+else if(arr == =3)
+{
 console.log('Argument found');
+}
+else
 console.log('Arguments found');
