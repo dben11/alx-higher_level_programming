0744d208c439d2f87caf6b0078289c44bc64cfbd
dben <sam.dben@gmail.com>
2023-11-16 01:29:33 -0800

    loop languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const lines = ["C is fun", "Python is cool", "Javascript is amazing"];
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
 }
