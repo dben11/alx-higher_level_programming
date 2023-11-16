697d463a926d2fa0eaa6047cb63036bbd31b3ece
dben <sam.dben@gmail.com>
2023-11-16 02:46:10 -0800

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@ const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; r < size; i++) {
+  for (let i = 0; i < size; i++) {
     let row = '';
-    for (let j = 0; c < size; j++) row += 'X';
+    for (let j = 0; j < size; j++) row += 'X';
     console.log(row);
   }
 }
