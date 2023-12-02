87d24412a86dbdf2d3e3d3f6027236f30e93b81b
dben <sam.dben@gmail.com>
2023-12-01 17:10:08 -0800

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """write a python script that fetches the following url"""
-
-if __name__ == "__main__":
 import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')as response:
-  content =response.read()
-  print("BOdy:")
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read().decode('utf-8')
+    print(body)
+
 
